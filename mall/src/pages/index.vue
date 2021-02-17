c712577b2d1e17b8755ef7d713bf0f87b5c1718f
WangSc0609 <1657924217@qq.com>
2021-02-17 16:25:09 +0800

    首页广告位

--- mall/src/pages/index.vue
+++ mall/src/pages/index.vue
@@ -54,8 +54,17 @@
         </swiper>
       </div>
       <!--广告位-->
-      <div class="ads-box"></div>
-      <div class="banner"></div>
+      <div class="ads-box">
+        <a :href="'/#/product/'+item.id" v-for="(item,index) in adsList" :key="index">
+          <img :src="item.img" alt="">
+        </a>
+      </div>
+      <!--banner-->
+      <div class="banner">
+        <a href="/#/product/30">
+        <img src="/imgs/banner-1.png" alt=""></a>
+      </div>
+      <!--商品列表-->
       <div class="product-box"></div>
     </div>
     <service-bar></service-bar>
@@ -137,6 +146,21 @@ export default {
           }
         ],
         [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]
+      ],
+      adsList:[
+        {
+          id:33,
+          img:'/imgs/ads/ads-1.png'
+        },{
+          id:48,
+          img:'/imgs/ads/ads-2.jpg'
+        },{
+          id:45,
+          img:'/imgs/ads/ads-3.png'
+        },{
+          id:47,
+          img:'/imgs/ads/ads-4.jpg'
+        }
       ]
     }
   }
@@ -225,6 +249,19 @@ export default {
           height:451px;
         }
       }
+    }
+    .ads-box{
+      @include flex();
+      margin-top: 14px;
+      margin-bottom: 31px;
+      a{
+        width:296px;
+        height:167px;
+      }
+    }
+
+    .banner{
+      margin-bottom:50px;
     }
   }
 </style>
