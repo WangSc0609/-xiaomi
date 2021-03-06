365337189437cad6515e8962bf8b292c5347b357
WangSc0609 <1657924217@qq.com>
2021-03-06 12:02:01 +0800

    微信与支付宝支付接口对接

--- mall/src/pages/alipay.vue
+++ mall/src/pages/alipay.vue
@@ -1,12 +1,42 @@
 <template>
-  <div>
-    alipay
+  <div class="ali-pay">
+    <loading v-if="loading"></loading>
+    <div class="form" v-html="content"></div>
   </div>
 </template>
 
 <script>
+import Loading from './../components/Loading'
 export default {
-  name:'alipay'
+  name:'alipay',
+  data(){
+    return {
+      orderId:this.$route.query.orderId,
+      content:'',
+      loading:true
+    }
+  },
+  components:{
+    Loading
+  },
+  mounted(){
+    this.paySubmit()
+  },
+  methods:{
+    paySubmit(){
+      this.axios.post('/pay',{
+        orderId:this.orderId,
+        orderName:'别付款！看看就行了！', //扫码支付时订单名称
+        amount:0.01,  //单位元
+        payType:1 //1支付宝，2微信
+      }).then((res)=>{
+        this.content=res.content;
+        setTimeout(()=>{
+          document.forms[0].submit();
+        },100)
+      })
+    }
+  }
 }
 </script>
 
